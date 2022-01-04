df23e588137350a5841927049ae8c83b8aa239e4
manuelRestrep0 <94997816+manuelRestrep0@users.noreply.github.com>
2022-01-03 20:06:58 -0500

    Update main.java
    implementacion del menu

--- reto_4_JUANMANUELRESTREPO/src/main/java/com/mycompany/reto_4_juanmanuelrestrepo/main.java
+++ reto_4_JUANMANUELRESTREPO/src/main/java/com/mycompany/reto_4_juanmanuelrestrepo/main.java
@@ -1,7 +1,44 @@
 package com.mycompany.reto_4_juanmanuelrestrepo;
+import java.util.Scanner;
 public class main {
     public static void main(String[] args)
     {
+        /*
+        Menú de la aplicacion
+        */
+        boolean ejecucion = true;
+        while(ejecucion)
+        {
+            int opcion;
+            Scanner input = new Scanner(System.in);
+            opcion = input.nextInt();
+            switch(opcion){
+                case 1:
+                {
+                    /*
+                    Registro de autos
+                    */
+                }
+                case 2:
+                {
+                    /*
+                    Alquiler de autos
+                    */
+                }
+                case 3:
+                {
+                    /*
+                    Seguros
+                    */
+                }
+                default:
+                {
+                    ejecucion = false;
+                }
+                    
+                    
+            }
+        }
         
     }
 }
